765adbe706aba6a6a0c58b9cd1744fe17be54906
sean-conkie <sean.conkie@sky.uk>
2024-05-20 09:33:56 +0100

    update extension to use mutation observer
    GitHub uses pjax so pages are updated within container, this meant extension only ran on refresh.

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,6 @@
 
-const navBar = document.getElementsByClassName('gh-header-actions')[0]
+const app = document.getElementsByClassName('application-main')[0]
+
 
 function switchDiscussionStyle(style) {
   const discussion = document.getElementsByClassName('js-discussion')[0]
@@ -17,6 +18,7 @@ function switchDiscussionStyle(style) {
  */
 function createButton() {
   const button = document.createElement('button')
+  button.id = 'id-button-show-commits'
   button.innerText = 'Show commits'
   button.classList.add('Button', 'Button--secondary', 'Button--small', 'flex-md-order-2', 'm-0', 'mr-md-0', 'active')
   button.onclick = function() {
@@ -34,7 +36,35 @@ function createButton() {
   return button
 }
 
-if (navBar) {
-  navBar.appendChild(createButton())
-  switchDiscussionStyle('display: none;')
+if (app) {
+
+  // Select the node that will be observed for mutations
+  const targetNode = app;
+
+  // Options for the observer (which mutations to observe)
+  const config = { attributes: true, childList: true, subtree: true };
+
+  // Callback function to execute when mutations are observed
+  const callback = (mutationList, observer) => {
+    
+    const progress = document.getElementsByClassName('turbo-progress-bar')[0]
+    if (!progress) {
+      const navBar = document.getElementsByClassName('gh-header-actions')[0]
+      if (navBar) {
+        // if the button does not exist, create it
+        if (!document.getElementById('id-button-show-commits')) {
+          navBar.appendChild(createButton())
+          // hide the comments
+          switchDiscussionStyle('display: none;')
+        }
+      }
+    }
+  };
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback);
+
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config);
+
 }
